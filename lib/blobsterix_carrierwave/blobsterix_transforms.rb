abfcaea0f6350dfad334d03385aa0f5429b2a3c8
Peter Schrammel <peter.schrammel@experteer.com>
2014-03-14 09:09:29 +0100

    renaming resize-max to resizemax (f* name:  _ and - are bad have to think about a godd separationchar)

--- lib/blobsterix_carrierwave/blobsterix_transforms.rb
+++ lib/blobsterix_carrierwave/blobsterix_transforms.rb
@@ -14,7 +14,7 @@ module BlobsterixTransforms
      width=args[0]
      height=args[1]
      {
-      :method => "resize-max",
+      :method => "resizemax",
       :args => "#{width}x#{height}"
      }
   end
